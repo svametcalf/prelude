8ad5b71ec273edcd57fb916a0c03df509d13dc63
Schuyler Metcalf <svametcalf@gmail.com>
2019-11-23 08:59:59 -0900

    adds hook to format elixir files

---
+++ personal/elixir-mode.el
@@ -0,0 +1,10 @@
+;; Create a buffer-local hook to run elixir-format on save, only when we enable elixir-mode.
+(add-hook 'elixir-mode-hook
+          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+
+(add-hook 'elixir-format-hook (lambda ()
+                                (if (projectile-project-p)
+                                    (setq elixir-format-arguments
+                                          (list "--dot-formatter"
+                                                (concat (locate-dominating-file buffer-file-name ".formatter.exs") ".formatter.exs")))
+                                  (setq elixir-format-arguments nil))))
