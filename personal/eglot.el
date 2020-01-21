d113b9c1bb82c3af18aeca9bdb1540d05efb5876
Schuyler Metcalf <svametcalf@gmail.com>
2020-01-20 19:30:20 -0900

    adds configuration for language servers!

---
+++ personal/eglot.el
@@ -0,0 +1,9 @@
+(prelude-require-package 'eglot)
+(require 'eglot)
+(add-to-list 'eglot-server-programs '(elixir-mode "/home/schuyler/elixir-ls/release/language_server.sh"))
+
+(add-hook 'elixir-mode-hook 'eglot-ensure)
+
+(define-key eglot-mode-map (kbd "C-c h") 'eglot-help-at-point)
+(define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions)
+(define-key eglot-mode-map (kbd "M-<f6>") 'eglot-rename)
