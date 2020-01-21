8ce0566ebf19acc1f9384eec629f8bca8eff71ad
Schuyler Metcalf <svametcalf@gmail.com>
2020-01-20 19:59:48 -0900

    adds more keybindings for LSP

--- personal/eglot.el
+++ personal/eglot.el
@@ -6,4 +6,6 @@
 
 (define-key eglot-mode-map (kbd "C-c h") 'eglot-help-at-point)
 (define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions)
+(define-key eglot-mode-map (kbd "M-RET") 'completion-at-point)
 (define-key eglot-mode-map (kbd "M-<f6>") 'eglot-rename)
+(define-key eglot-mode-map (kbd "M-?") 'xref-find-references)
